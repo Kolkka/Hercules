1cf43d2911b465e33f021f8921b511bc1147942d
Haru <haru@dotalux.com>
2016-02-20 14:43:20 +0100

    Dropped typedef from DBIterator
    Signed-off-by: Haru <haru@dotalux.com>

--- src/map/script.c
+++ src/map/script.c
@@ -4216,8 +4216,9 @@ void run_script(struct script_code *rootscript, int pos, int rid, int oid) {
 	script->run_main(st);
 }
 
-void script_stop_instances(struct script_code *code) {
-	DBIterator *iter;
+void script_stop_instances(struct script_code *code)
+{
+	struct DBIterator *iter;
 	struct script_state* st;
 
 	if( !script->active_scripts )
@@ -4678,9 +4679,10 @@ void script_generic_ui_array_expand (unsigned int plus) {
 /*==========================================
  * Destructor
  *------------------------------------------*/
-void do_final_script(void) {
+void do_final_script(void)
+{
 	int i;
-	DBIterator *iter;
+	struct DBIterator *iter;
 	struct script_state *st;
 
 #ifdef SCRIPT_DEBUG_HASH
@@ -4871,8 +4873,7 @@ void script_load_translations(void) {
 	libconfig->destroy(&translations_conf);
 
 	if( total ) {
-		DBIterator *main_iter;
-		DBIterator *sub_iter;
+		struct DBIterator *main_iter, *sub_iter;
 		DBMap *string_db;
 		struct string_translation *st = NULL;
 		uint32 j = 0;
@@ -5093,7 +5094,7 @@ int script_translation_db_destroyer(union DBKey key, struct DBData *data, va_lis
 
 	if( db_size(string_db) ) {
 		struct string_translation *st = NULL;
-		DBIterator *iter = db_iterator(string_db);
+		struct DBIterator *iter = db_iterator(string_db);
 
 		for( st = dbi_first(iter); dbi_exists(iter); st = dbi_next(iter) ) {
 			aFree(st);
@@ -5172,9 +5173,10 @@ void do_init_script(bool minimal) {
 	script->load_translations();
 }
 
-int script_reload(void) {
+int script_reload(void)
+{
 	int i;
-	DBIterator *iter;
+	struct DBIterator *iter;
 	struct script_state *st;
 
 #ifdef ENABLE_CASE_CHECK
@@ -16947,8 +16949,9 @@ BUILDIN(sleep2) {
 /// Awakes all the sleep timers of the target npc
 ///
 /// awake "<npc name>";
-BUILDIN(awake) {
-	DBIterator *iter;
+BUILDIN(awake)
+{
+	struct DBIterator *iter;
 	struct script_state *tst;
 	struct npc_data* nd;
 
